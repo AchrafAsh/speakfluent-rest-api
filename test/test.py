ae86acf8440dffc6149bcb1fab56f7dfc21df7b8
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-10-27 22:18:53 +0100

    remove audio and test

--- test/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-headers = { "content-type": "application/json" }
-
-response = requests.post("http://localhost:5000/predict", headers=headers, data={ "filename": "0001.wav", "word": "have" })
-
-print(response.text)
