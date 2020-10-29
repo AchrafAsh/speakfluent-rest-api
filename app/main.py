fe74cddc15ca7c4729650144b1e68bbff2eff51c
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-10-29 18:30:08 +0100

    setup to deploy

--- app/main.py
+++ app/main.py
@@ -121,3 +121,8 @@ def _build_cors_prelight_response():
 def _corsify_actual_response(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
